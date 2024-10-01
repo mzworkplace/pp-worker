58adff551c98b2cebc2e797f8777fefcb209020d
mzworkplace <moazamiworkplace@gmail.com>
2024-10-01 12:40:30 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ import { connect } from 'cloudflare:sockets';
 // t.me/P_tech2024 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = '06301be8-1561-46bc-a476-032d309886e2';
+let userID = '754308b6-15e2-48db-8d5a-11aec6755139';
 
 const proxyIPs = ['mtn.ircf.space', 'mkh.ircf.space', 'mci.ircf.space', 'rtl.ircf.space'];
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
